5f84b93ca8248244d7cab15cfd47acbc0e4f1854
Plutonium <plu-239@yandex.ru>
2014-09-29 14:03:10 +0400

    Ввод регистрационных данных переделан на использование PreferenceActivity. Ключи форм и имен настроек вынесены в keys.xml. Рефакторинг @string-имен.

---
+++ src/main/scala/com/pirotehnika_ruhelp/chat/PreferenceActivity.scala
@@ -0,0 +1,11 @@
+package com.pirotehnika_ruhelp.chat
+
+import android.os.Bundle
+import android.preference.{PreferenceActivity => AndroidPrefActivity}
+
+class PreferenceActivity extends AndroidPrefActivity {
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    addPreferencesFromResource(R.layout.preferences)
+  }
+}
