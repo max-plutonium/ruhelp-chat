9ce4b99d785c6d29b8b32196afa63ab781265ec3
Plutonium <plu-239@yandex.ru>
2014-09-29 18:01:21 +0400

    Вход-выход пользователя через пункт меню.

--- src/main/scala/com/pirotehnika_ruhelp/chat/MainActivity.scala
+++ src/main/scala/com/pirotehnika_ruhelp/chat/MainActivity.scala
@@ -1,6 +1,6 @@
 package com.pirotehnika_ruhelp.chat
 
-import java.net.UnknownHostException
+import java.io.IOException
 import java.util.Map
 
 import android.app.Activity
@@ -9,50 +9,60 @@ import android.net.{ConnectivityManager, Uri}
 import android.os.{AsyncTask, Bundle}
 import android.preference.PreferenceManager
 import android.util.Log
+import android.view.MenuItem.OnMenuItemClickListener
 import android.view.{Menu, MenuItem, View}
 import android.widget.{Button, TextView, Toast}
 import org.jsoup.{Connection, Jsoup}
 
-class MainActivity extends Activity with View.OnClickListener {
+class MainActivity extends Activity {
+  import com.pirotehnika_ruhelp.chat.MainActivity._
   private lazy val textView = findViewById(R.id.chatText).asInstanceOf[TextView]
-  private lazy val btnEnter = findViewById(R.id.chatEnter).asInstanceOf[Button]
-  private lazy val btnExit = findViewById(R.id.chatExit).asInstanceOf[Button]
   private lazy val prefs = PreferenceManager getDefaultSharedPreferences this
   private var worker: Worker = null
-  import com.pirotehnika_ruhelp.chat.MainActivity._
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
-    btnEnter setOnClickListener this
-    btnExit setOnClickListener this
     self = this
   }
 
   override def onResume(): Unit = {
-    if(isNetworkAvailable) btnEnter setText R.string.chat_enter
-    else btnEnter setText R.string.chat_nonetwork
     super.onResume()
   }
 
-  override def onClick(v: View): Unit = v.getId match {
-    case R.id.chatEnter => if(worker eq null) {
-      worker = new Worker(enterUrl, chatUrl)
-      worker execute()
-    }
-    case R.id.chatExit => finish() // TODO
-  }
-
-  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+  override def onCreateOptionsMenu(menu: Menu) = {
     getMenuInflater inflate(R.menu.main, menu)
+    val mi = menu findItem R.id.menu_prefs
+    assert(mi ne null)
+    mi setIntent new Intent(this, classOf[PreferenceActivity])
     super.onCreateOptionsMenu(menu)
   }
 
-  override def onOptionsItemSelected(item: MenuItem): Boolean =
-    item.getItemId match {
-      case R.id.menu_prefs => startActivity(new Intent(this, classOf[PreferenceActivity]))
-        super.onOptionsItemSelected(item)
+  override def onPrepareOptionsMenu(menu: Menu): Boolean = {
+    val mi = menu findItem R.id.menu_signing
+    assert(mi ne null)
+    if(userEntered) {
+      mi setTitle R.string.chat_menu_sign_off
+      mi setOnMenuItemClickListener new OnMenuItemClickListener {
+        override def onMenuItemClick(item: MenuItem): Boolean = {
+          finish() // TODO
+          true
+        }
+      }
+    } else {
+      mi setTitle R.string.chat_menu_sign_on
+      mi setOnMenuItemClickListener new OnMenuItemClickListener {
+        override def onMenuItemClick(item: MenuItem): Boolean = {
+          worker = new Worker(enterUrl, chatUrl)
+          worker execute()
+          true
+        }
+      }
     }
+
+    mi setEnabled(worker == null)
+    super.onCreateOptionsMenu(menu)
+  }
 
   // See http://piotrbuda.eu/2012/12/scala-and-android-asynctask-implementation-problem.html
   private class Worker(val loginUrl: Uri, var chatUrl: Uri)
@@ -66,14 +76,13 @@ class MainActivity extends Activity with View.OnClickListener {
     override protected def onPreExecute(): Unit = {
       super.onPreExecute()
       assert(worker ne null)
-      btnEnter setEnabled false
     }
 
     override protected def doInBackground(params: AnyRef*): LoginResult = {
       var resp: Connection.Response = null
 
       try {
-        Log i(TAG, "Connecting to " + loginUrl)
+        Log i(TAG, "Request login info from " + loginUrl)
         resp = Jsoup.connect(loginUrl toString)
           .method(Connection.Method.GET).execute()
         Log i(TAG, "Connected to " + loginUrl)
@@ -89,7 +98,7 @@ class MainActivity extends Activity with View.OnClickListener {
         val method = if (form.attr("method").toUpperCase.equals("POST"))
           Connection.Method.POST else Connection.Method.GET
 
-        Log i(TAG, "Send registration info to " + action)
+        Log i(TAG, "Send login info to " + action)
         resp = Jsoup.connect(action)
           .data(getString(R.string.key_form_auth), authKey)
           .data(getString(R.string.key_form_referrer), chatUrl toString)
@@ -101,28 +110,28 @@ class MainActivity extends Activity with View.OnClickListener {
 
         Log i(TAG, "Connected to " + action)
         Log i(TAG, "Status code [" + resp.statusCode + "] - " + resp.statusMessage)
-
-        val body = resp.body
+        Log i(TAG, "Login successful")
         LoginResult(authKey, resp.cookies)
 
-      } catch {
-        case e: UnknownHostException => e.printStackTrace()
-          LoginResult(null, null)
-        case e: Throwable => e.printStackTrace()
+      } catch { case e: IOException =>
+          Log e(TAG, "Login failure, caused: " + e.getMessage)
+          e.printStackTrace()
           LoginResult(null, null)
       }
     }
 
     override protected def onPostExecute(result: LoginResult): Unit = {
-      result match {
+      userEntered = result match {
         case LoginResult(null, null) =>
           Toast makeText(MainActivity.this, R.string.chat_error_network, Toast.LENGTH_LONG) show()
+          false
         case LoginResult(a, c) =>
           authKey = a
           cookies = c
+          Toast makeText(MainActivity.this, R.string.chat_user_login, Toast.LENGTH_LONG) show()
+          true
       }
       worker = null
-      btnEnter setEnabled true
       super.onPostExecute(result)
     }
   }
@@ -139,6 +148,7 @@ object MainActivity {
   private[MainActivity] case class LoginResult(authKey: String, cookies: Map[String, String])
   private[MainActivity] var authKey = ""
   private[MainActivity] var cookies: Map[String, String] = null
+  private[MainActivity] var userEntered = false
 
   private[chat] def isNetworkAvailable: Boolean = {
     val cm = self.getSystemService(Context.CONNECTIVITY_SERVICE).asInstanceOf[ConnectivityManager]
